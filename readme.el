788803074ae79520070ed75a09825989c56d0d06
aidanprior <priorlyaidan@yahoo.com>
2025-09-12 16:44:07 -0400

    fix(readme.el): clone submodules (should not matter?) and correct loading paths

--- readme.el
+++ readme.el
@@ -1,8 +1,8 @@
 ;; readme.el  -*- lexical-binding: t; -*-
-(let ((dir (file-name-directory load-file-name)))
-  (load (concat dir "ox-gfm/ox-gfm"))
-  (load (concat dir "ox-md-title/ox-md-title"))
-  (load (concat dir "ox-gfm-alerts.el")))
+(let ((dir (or (getenv "GITHUB_WORKSPACE") default-directory)))
+  (load (expand-file-name "ox-gfm/ox-gfm.el" dir))
+  (load (expand-file-name "ox-md-title/ox-md-title.el" dir))
+  (load (expand-file-name "ox-gfm-alerts.el" dir)))
 
 (require 'ox-gfm)
 (require 'ox-md-title)
